19fcb9c5c0411652fb6c0553f8b65282fea4d1f2
aleks2414 <alejandroromo14@gmail.com>
2017-07-24 14:45:32 -0500

    divse

---
+++ db/migrate/20170724193450_add_attributes_to_user.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :puesto, :string
+    add_column :users, :phone, :string
+    add_column :users, :admin, :boolean
+  end
+end
