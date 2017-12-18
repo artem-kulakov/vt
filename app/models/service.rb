8b2edb041f06de4042b2d781bad154385f65813a
aleks2414 <alejandroromo14@gmail.com>
2017-12-18 09:51:35 -0600

    registros

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,7 +4,7 @@ class Service < ActiveRecord::Base
   belongs_to :user
 
   def sum_gastos
-    (self.try(:hotel)+self.try(:caseta)+self.try(:viaticos)+self.try(:estacionamientos)+self.try(:lavadas)+self.try(:aeropuerto)+self.try(:diesel)+self.try(:otros)).round(2)
+    @sum_gastos= (self.try(:hotel)+self.try(:caseta)+self.try(:viaticos)+self.try(:estacionamientos)+self.try(:lavadas)+self.try(:aeropuerto)+self.try(:diesel)+self.try(:otros)).round(2)
   end
 
   def resourceId
