89bbd94079740923cf8600d7b0487392a33e5672
Alex Romo <alejandroromo14@gmail.com>
2020-02-15 14:26:47 -0600

    year test4

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,7 +9,7 @@ class ServicesController < ApplicationController
     @q = Service.ransack(params[:q])
     @services = @q.result.uniq
     @services = @services.order('id ASC')
-    @services = @services.where("created_at >= ?", Time.zone.now.beginning_of_day-1000)
+    @services = @services.where("created_at >= ?", Time.zone.now.beginning_of_day-10000)
   end
 
   # GET /services/1
