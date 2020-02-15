962f5d787c5dfc325f75fee7be275308c90c1a14
Alex Romo <alejandroromo14@gmail.com>
2020-02-15 14:20:16 -0600

    year test4

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,7 +9,7 @@ class ServicesController < ApplicationController
     @q = Service.ransack(params[:q])
     @services = @q.result.uniq
     @services = @services.order('id ASC')
-    @services = @services.where("created_at < ?", 1.year.ago)
+    @services = @services.where(created_at: 1.year.ago.beginning_of_year..Time.now.beginning_of_year)
   end
 
   # GET /services/1
