042b84bc364dd4ced303f0964d6dc63ada179dda
Artem Kulakov <artem.kulakov@gmail.com>
2020-02-24 17:21:08 +0300

    /reporte: show only one (random) month

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,11 +12,13 @@ class WelcomeController < ApplicationController
   def reporte
     @years = Service.select("created_at").map{ |i| i.created_at.year }.uniq.sort
     @year = params[:year] || Date.current.year
-    date = DateTime.new(@year.to_i, 6, 30) # just a date in the middle of the year
+    date = DateTime.new(@year.to_i, 2, 15) # just a date in the middle of the month
 
-  	@services = Service.all
+  	@services = Service
+      .where("fecha > :start AND fecha < :end", {start: date.beginning_of_month, end: date.end_of_month})
   	@buses = Bus.all
-  	@receipts = Receipt.all
+  	@receipts = Receipt
+      .where("fecha > :start AND fecha < :end", {start: date.beginning_of_month, end: date.end_of_month})
   	@gas = Ga.all
   	@costs = @services + @receipts
   end
