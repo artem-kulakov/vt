f81a65e43cb9428bba2ad7ce0f3b4308d0828139
Artem Kulakov <artem.kulakov@gmail.com>
2020-02-24 17:53:51 +0300

    /reporte: add month tabs and show selected month only

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,9 +11,15 @@ class WelcomeController < ApplicationController
 
   def reporte
     @years = Service.select("created_at").map{ |i| i.created_at.year }.uniq.sort
-
     @year = params[:year] || Date.current.year
-    @month = params[:month] || Date.current.month
+
+    @months = Array(1..12)
+    if @year.to_s == Date.current.year.to_s
+      @month = params[:month] || Date.current.month
+    else
+      @month = 12
+    end
+
     date = DateTime.new(@year.to_i, @month.to_i, 15) # just a date in the middle of the month
 
   	@services = Service
