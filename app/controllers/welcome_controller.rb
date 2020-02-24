308ee123af9dd947e1cb2d22641a575c7744f312
Artem Kulakov <artem.kulakov@gmail.com>
2020-02-24 17:07:00 +0300

    /reporte: remove pagination (it doesn't working properly with /reporte)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,8 +19,5 @@ class WelcomeController < ApplicationController
   	@receipts = Receipt.all
   	@gas = Ga.all
   	@costs = @services + @receipts
-    # @costs = @costs
-    #   .paginate(:page => params[:page], :per_page => 20)
-      # .where("created_at > :start AND created_at < :end", {start: date.beginning_of_year, end: date.end_of_year})
   end
 end
