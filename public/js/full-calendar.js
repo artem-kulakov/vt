77594893d1b22ff4a55065fb77f0d3c486d858f3
Artem Kulakov <artem.kulakov@gmail.com>
2020-03-27 23:54:06 +0300

    Remove events from js

--- public/js/full-calendar.js
+++ public/js/full-calendar.js
@@ -1,15 +1,4 @@
 $(window).on('load', function() {
-    $('#external-events .fc-event').each(function() {
-        $(this).data('event', {
-            title: $.trim($(this).text()),
-            stick: true
-        });
-        $(this).draggable({
-            zIndex: 999,
-            revert: true,
-            revertDuration: 0
-        });
-    });
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
