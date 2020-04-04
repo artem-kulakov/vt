f87e87b1693581ddb0dc9a959a6862d0818a5659
Artem Kulakov <artem.kulakov@gmail.com>
2020-04-04 13:56:04 +0300

    Services chart prototype

--- public/js/charts.js
+++ public/js/charts.js
@@ -169,30 +169,119 @@ $(document).ready(function() {
     });
 
 
-    // Servicios
-    var bar = document.getElementById("chart-line-2").getContext('2d');
-    var theme_g2 = bar.createLinearGradient(0, 0, 500, 0);
-    theme_g2.addColorStop(0, '#899FD4');
-    theme_g2.addColorStop(1, '#A389D4');
-    var data1 = {
-        labels: data.services.labels,
-        datasets: [{
-            label: "Servicios Mensuales",
-            data: data.services.data,
-            fill: false,
-            borderWidth: 4,
-            borderColor: theme_g2,
-            backgroundColor: theme_g2,
-            hoverborderColor: theme_g2,
-            hoverBackgroundColor: theme_g2
-        }]
-    };
-    var myBarChart = new Chart(bar, {
-        type: 'line',
-        data: data1,
-        responsive: true,
-        options: line_chart_options
+    // Services
+    var chartDatad = data.quotations;
+    var chartd = AmCharts.makeChart("services-chart", {
+        "type": "serial",
+        "addClassNames": true,
+        "defs": {
+            "filter": [{
+                    "x": "-50%",
+                    "y": "-50%",
+                    "width": "200%",
+                    "height": "200%",
+                    "id": "blur",
+                    "feGaussianBlur": {
+                        "in": "SourceGraphic",
+                        "stdDeviation": "30"
+                    }
+                },
+                {
+                    "id": "shadow",
+                    "x": "-10%",
+                    "y": "-10%",
+                    "width": "120%",
+                    "height": "120%",
+                    "feOffset": {
+                        "result": "offOut",
+                        "in": "SourceAlpha",
+                        "dx": "0",
+                        "dy": "20"
+                    },
+                    "feGaussianBlur": {
+                        "result": "blurOut",
+                        "in": "offOut",
+                        "stdDeviation": "10"
+                    },
+                    "feColorMatrix": {
+                        "result": "blurOut",
+                        "type": "matrix",
+                        "values": "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .2 0"
+                    },
+                    "feBlend": {
+                        "in": "SourceGraphic",
+                        "in2": "blurOut",
+                        "mode": "normal"
+                    }
+                }
+            ]
+        },
+        "fontSize": 12,
+        "dataProvider": chartDatad,
+        "autoMarginOffset": 0,
+        "marginRight": 0,
+        "categoryField": "year",
+        "categoryAxis": {
+            "color": '#fff',
+            "gridAlpha": 0,
+            "axisAlpha": 0,
+            "lineAlpha": 0,
+            "offset": -20,
+            "inside": true,
+            "parseDates": false,
+            "autoGridCount": false,
+            "gridCount": 12,
+            "minPeriod": "YYYY"
+        },
+        "valueAxes": [{
+            "fontSize": 0,
+            "inside": true,
+            "gridAlpha": 0,
+            "axisAlpha": 0,
+            "lineAlpha": 0,
+            "minimum": 0,
+        }],
+        "chartCursor": {
+            "valueLineEnabled": false,
+            "valueLineBalloonEnabled": false,
+            "cursorAlpha": 0,
+            "zoomable": false,
+            "valueZoomable": false,
+            "cursorColor": "#fff",
+            "categoryBalloonColor": "#95ead5",
+            "valueLineAlpha": 0
+        },
+        "graphs": [{
+            "id": "g1",
+            "type": "line",
+            "valueField": "value",
+            "fillColors": [
+                "#1de9b6",
+                "#1dc4e9"
+            ],
+            "lineAlpha": 0,
+            "fillAlphas": 1,
+            "showBalloon": true,
+            "type": "smoothedLine",
+            "balloon": {
+                "drop": true,
+                "adjustBorderColor": false,
+                "color": "#ffffff",
+                "fillAlphas": 0.2,
+                "bullet": "round",
+                "bulletBorderAlpha": 1,
+                "bulletSize": 5,
+                "hideBulletsCount": 50,
+                "lineThickness": 2,
+                "useLineColorForBulletBorder": true,
+                "valueField": "value",
+                "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
+            },
+        }, ],
     });
+    setTimeout(function() {
+        chartd.zoomToIndexes(1, chartDatad.length - 2);
+    }, 400);
 
 
     // Gastos
