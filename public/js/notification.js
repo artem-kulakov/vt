9ef1e154b9b919c61ccf368a8e2ac259e156ccc4
Artem Kulakov <artem.kulakov@gmail.com>
2020-04-11 12:42:05 +0300

    Notice is done

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,24 +1,17 @@
 $( document ).ready(function() {
-  if ($('#notification').length > 0) {
-    // alert('bar');
-    // var x = document.getElementById("notification");
+  var notice = $('#notification');
 
-    // x.className = "show";
-
-    // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-
-        (new PNotify({
-            title: 'Danger Desktop Notice',
-            type: 'danger',
-            text: 'I\'m a danger desktop notification, if you have given me a permission.',
-            desktop: {
-                desktop: true,
-                icon: 'assets/images/pnotify/danger.png'
-            }
-        })
-        ).get().click(function(e) {
-            if ($('.ui-pnotify-closer, .ui-pnotify-sticker, .ui-pnotify-closer *, .ui-pnotify-sticker *').is(e.target)) return;
-            alert('Hey! You clicked the desktop notification!');
-        });
+  if (notice.length > 0) {
+    (new PNotify({
+        title: 'Notice',
+        type: 'success',
+        delay: 3000,
+        text: notice.text(),
+        desktop: {
+            desktop: true,
+            icon: 'assets/images/pnotify/success.png'
+        }
+    })
+    );
   }
 });
