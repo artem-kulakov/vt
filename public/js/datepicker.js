f49cd74b0cb8298d14544a6d2b69438fd5564483
Artem Kulakov <artem.kulakov@gmail.com>
2020-04-11 19:45:10 +0300

    Turn autocomplete off

---
+++ public/js/datepicker.js
@@ -0,0 +1,8 @@
+$(window).ready(function() {
+    $('.d_today').datepicker({
+        keyboardNavigation: false,
+        forceParse: false,
+        todayHighlight: true,
+        format: 'dd/mm/yyyy'
+    });
+});
