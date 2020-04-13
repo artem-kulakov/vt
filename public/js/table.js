9adb8b41cce42f2968f66476d0ef19444cb0e88f
Artem Kulakov <artem.kulakov@gmail.com>
2020-04-14 00:57:34 +0300

    Remove sorting from Ver mas, Editar, Eliminar

--- public/js/table.js
+++ public/js/table.js
@@ -3,6 +3,26 @@ $(document).ready(function() {
         "order": [[ 0, "asc" ]],
         "paging": false,
         "searching": false,
+        "bInfo" : false,
+        "columnDefs": [
+           { "orderable": false, "targets": [-1,-2] }
+        ]
+    } );
+
+    $('#example-3').DataTable( {
+        "order": [[ 0, "asc" ]],
+        "paging": false,
+        "searching": false,
+        "bInfo" : false,
+        "columnDefs": [
+           { "orderable": false, "targets": [-1,-2,-3] }
+        ]
+    } );
+
+    $('#example-0').DataTable( {
+        "order": [[ 0, "asc" ]],
+        "paging": false,
+        "searching": false,
         "bInfo" : false
     } );
 
@@ -10,6 +30,9 @@ $(document).ready(function() {
         "order": [[ 0, "asc" ]],
         "paging": false,
         "searching": false,
-        "bInfo" : false
+        "bInfo" : false,
+        "columnDefs": [
+           { "orderable": false, "targets": [-1,-2] }
+        ]
     } );
 } );
